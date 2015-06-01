bff17c54cd04aa30cbce765f8a0771afcc8f138c
Arpit Singh <arpitsinghnitd@gmail.com>
2015-06-01 15:41:53 +0530

    Commit

---
+++ app/src/main/java/com/example/root/broadcastreceivernewsms/BroadcastNewSms.java
@@ -0,0 +1,73 @@
+package com.example.root.broadcastreceivernewsms;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.telephony.gsm.SmsManager;
+import android.telephony.gsm.SmsMessage;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URL;
+
+
+public class BroadcastNewSms extends Activity implements View.OnClickListener {
+    public static String message;
+    public static String url;
+    private static Context context;
+    EditText et;
+    Button b;
+    Context c;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcast_new_sms);
+        et = (EditText) findViewById(R.id.editText1);
+        b = (Button) findViewById(R.id.button1);
+        b.setOnClickListener(this);
+        BroadcastNewSms.context=getApplicationContext();
+    }
+
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        if (et.getText().toString().length() < 1) {
+
+            // out of range
+            Toast.makeText(this, "please enter something", Toast.LENGTH_LONG).show();
+        } else {
+            url = et.getText().toString();
+
+        }
+
+
+    }
+
+    public String fun1() {
+        return url;
+
+
+    }
+    public static Context getAppContext() {
+        return BroadcastNewSms.context;
+    }
+}
+
+
+
